a5e7fe765e087fae34ad49f4f6668f66d9dd4d40
Kat556 <81787663+Kat556@users.noreply.github.com>
2021-04-03 21:19:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,19 +17,14 @@ processed_features = []
 for sentence in range(0, len(features)):
     # Remove all the special characters
     processed_feature = re.sub(r'\W', ' ', str(features[sentence]))
-
     # remove all single characters
     processed_feature= re.sub(r'\s+[a-zA-Z]\s+', ' ', processed_feature)
-
     # Remove single characters from the start
     processed_feature = re.sub(r'\^[a-zA-Z]\s+', ' ', processed_feature)
-
     # Substituting multiple spaces with single space
     processed_feature = re.sub(r'\s+', ' ', processed_feature, flags=re.I)
-
     # Removing prefixed 'b'
     processed_feature = re.sub(r'^b\s+', '', processed_feature)
-
     # Converting to Lowercase
     processed_feature = processed_feature.lower()
 
