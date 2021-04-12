091124ab672f131edc12b4393e4246aac215537d
Saransh Kalra <ksaransh201@gmail.com>
2021-04-12 14:15:37 -0500

    different KNN k values and distances tested and graphed
    73.33% reached

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import sys
 import re
+import nltk
+import matplotlib.pyplot as plt
+nltk.download('stopwords')
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.preprocessing import StandardScaler
+sys.stdout = open('knnexample.txt','w')
 
 imdb_reviews = pd.read_csv('imdb_labelled.txt', sep='\t', header=None)
 imdb_reviews.columns = ['comments', 'ranking']
@@ -33,17 +39,27 @@ for sentence in range(0, len(features)):
 vectorizer = TfidfVectorizer(max_features=2500, min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
 processed_features = vectorizer.fit_transform(processed_features).toarray()
 
-X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(processed_features, labels, test_size=0.2, random_state=100)
 
 scaler = StandardScaler()
 scaler.fit(X_train)
 
-classifier = KNeighborsClassifier(n_neighbors=5)
-classifier.fit(X_train, y_train)
+accuracies = []
 
-y_prediction = classifier.predict(X_test)
+for i in range(2,151):
+    print('\nk =',i)
 
-print(confusion_matrix(y_test, y_prediction))
-print(classification_report(y_test, y_prediction))
-print(accuracy_score(y_test, y_prediction))
-
+    classifier = KNeighborsClassifier(n_neighbors=i, metric = 'euclidean')
+    classifier.fit(X_train, y_train)
+    
+    y_prediction = classifier.predict(X_test)
+    
+    print(confusion_matrix(y_test, y_prediction))
+    print(classification_report(y_test, y_prediction))
+    print(accuracy_score(y_test, y_prediction))
+    accuracies.append(accuracy_score(y_test, y_prediction))
+    
+curve = pd.DataFrame(accuracies, columns = ['accuracy'])
+curve.index.name = i
+a = curve.plot(title = 'graph')
+a.set_ylabel('accuracy_score')
